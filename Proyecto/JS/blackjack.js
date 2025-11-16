718ee34063b88806237ae8e1bfabaf6e301d9f58
Raul-S-L <sm23.rsanchez@iessantamariadeivissa.com>
2025-11-16 21:06:31 +0100

    v2.4

--- Proyecto/JS/blackjack.js
+++ Proyecto/JS/blackjack.js
@@ -1,13 +1,22 @@
 let baraja = [];
 const tipoCarta = ["C","D","P","T"];
+const especiales = [ "A","K","Q","J"];
 const creaBaraja = () => {
     for(let i =2; i <=10; i++) {
         for (let tipo of tipoCarta) {
             baraja.push(i + tipo);
         }
-        baraja.push(i +"C");
     }
+
+    for (let espe of especiales) {
+      for (let tipo of tipoCarta) {
+        baraja.push(espe + tipo);
+    }
+    }
+
+    baraja = _.shuffle(baraja);
     console.log(baraja);
+    return baraja;
 };
 
 crearBaraja();
